efb10e6f52b8f25f13c255991801534793b97cc1
ceciliavoronoff <42843738+ceciliavoronoff@users.noreply.github.com>
2019-12-12 16:56:27 -0300

    Add files via upload

--- hanoi.py
+++ hanoi.py
@@ -6,13 +6,36 @@ quadric = 0
 GLUquadricObj *quadric
 quadric = gluNewQuadric()
 pino1 = [8, 7, 6, 5, 4, 3, 2, 1]
+pino2 = []
+pino3 = []
 
-def desenhaDiscos():
+def desenhaDiscosA():
    z = 4.5
    for i in pino1:
       glPushMatrix()
       glTranslatef(0,0,z)
-      gluCylinder(quadric,i/5,i/5,0.5,32,32)
+      glColor3f(0.5,0,1/i)
+      gluCylinder(quadric,i/4,i/4,0.5,32,32)
+      z -= 0.5
+      glPopMatrix()
+
+def desenhaDiscosB():   
+   z = 4.5
+   for i in pino2:
+      glPushMatrix()
+      glTranslatef(0,0,z)
+      glColor3f(0.5,0,1/i)
+      gluCylinder(quadric,i/4,i/4,0.5,32,32)
+      z -= 0.5
+      glPopMatrix()
+   
+def desenhaDiscosC():   
+   z = 4.5
+   for i in pino3:
+      glPushMatrix()
+      glTranslatef(0,0,z)
+      glColor3f(0.5,0,1/i)
+      gluCylinder(quadric,i/4,i/4,0.5,32,32)
       z -= 0.5
       glPopMatrix()
 
@@ -21,18 +44,23 @@ def desenhaPinos():
    glPushMatrix()
    glRotatef(90,1,0,0)
    glTranslatef(0,0,-3)
+   glColor3f(1,1,1)
    gluCylinder(quadric,0.1,0.1,5.0,32,32)
+   desenhaDiscosB()
    glPopMatrix()
    glPushMatrix()
    glRotatef(90,1,0,0)
    glTranslatef(-5,0,-3)
+   glColor3f(1,1,1)
    gluCylinder(quadric,0.1,0.1,5.0,32,32)
-   desenhaDiscos()
+   desenhaDiscosA()
    glPopMatrix()
    glPushMatrix()
    glRotatef(90,1,0,0)
    glTranslatef(5,0,-3)
+   glColor3f(1,1,1)
    gluCylinder(quadric,0.1,0.1,5.0,32,32)
+   desenhaDiscosC()
    glPopMatrix()
    glutSwapBuffers()
 
